07187b1457b9de3de50e4e7ca21d2d3ad0af9647
Kirill <109174433+Kirillk1o@users.noreply.github.com>
2023-03-22 19:03:09 +0300

    1. RESTFul. Описание архитектуры [#6884 #403009]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -1,15 +1,14 @@
 package ru.job4j.auth.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import ru.job4j.auth.domain.Person;
-import ru.job4j.auth.repository.PersonRepository;
 import ru.job4j.auth.service.PersonService;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -43,15 +42,13 @@ public class PersonController {
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
-        personService.save(person);
+        personService.update(person);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
-        Person person = personService.getById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Person with id: " + id + " is not exists"));
-        personService.delete(person);
+        personService.delete(id);
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/ru/job4j/auth/service/PersonService.java
+++ src/main/java/ru/job4j/auth/service/PersonService.java
@@ -2,12 +2,13 @@ package ru.job4j.auth.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.repository.PersonRepository;
 
 import javax.persistence.EntityNotFoundException;
+
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -28,12 +29,22 @@ public class PersonService {
     return personRepository.findById(personId);
     }
 
-    public boolean delete(Person person) {
-        boolean rsl = false;
-        if (person == null) {
-            return rsl;
-        }
-      personRepository.delete(person);
-        return !rsl;
+    public boolean update(Person person) {
+        boolean result = false;
+        Person findPerson = personRepository.findById(person.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Person is not exists"));
+                if (!person.equals(findPerson)) {
+                    findPerson.setPassword(person.getPassword());
+                    personRepository.save(findPerson);
+                    return !result;
+                }
+                return result;
+    }
+
+    public boolean delete(int id) {
+        Person findPerson = personRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Person is not exists"));
+      personRepository.delete(findPerson);
+      return true;
     }
 }
